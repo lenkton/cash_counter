20ca3600b468e56da249415f92befdb3413a523d
lenkton <antonov_s@yahoo.com>
2021-08-28 02:15:04 +0500

    added procurement lot creation

--- app/controllers/procurement_lots_controller.rb
+++ app/controllers/procurement_lots_controller.rb
@@ -1,4 +1,24 @@
 class ProcurementLotsController < ApplicationController
   def index
   end
+
+  def new
+    @procurement_lot = ProcurementLot.new
+  end
+
+  def create
+    @procurement_lot = ProcurementLot.new(procurement_lot_params)
+
+    if @procurement_lot.save
+      redirect_to @procurement_lot
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def procurement_lot_params
+    params.require(:procurement_lot).permit(:title, :description)
+  end
 end
--- spec/controllers/procurement_lots_controller_spec.rb
+++ spec/controllers/procurement_lots_controller_spec.rb
@@ -1,11 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe ProcurementLotsController, type: :controller do
+  let(:user) { create :user }
+
+  before { log_in user }
+
   describe 'GET #index' do
     it 'responds with the index template' do
-      log_in create(:user)
       get :index
       expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'responds with the new template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid params' do
+      let(:valid_params) { { procurement_lot: { title: 'title', description: 'alala' } } }
+      it 'responds with OK' do
+        post :create, params: valid_params
+        expect(response).to redirect_to ProcurementLot.last
+      end
+
+      it 'creates a lot' do
+        expect { post :create, params: valid_params }.to change(ProcurementLot, :count).by(1)
+      end
+    end
+
+    context 'with invalid params' do
+      let(:invalid_params) { { procurement_lot: { title: '' } } }
+      it 'responds with unprocessible entity' do
+        post :create, params: invalid_params
+        expect(response).to be_unprocessable
+      end
+
+      it 'does not create a lot' do
+        expect { post :create, params: invalid_params }.not_to change(ProcurementLot, :count)
+      end
     end
   end
 end
